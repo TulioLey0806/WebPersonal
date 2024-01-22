c90ca1f995c54bf8c2065f9c956c82b3d6589ea4
TulioLey0806 <tulioley0806@gmail.com>
2024-01-22 11:58:57 -0500

    83. Usuario Controller - Metodo Accion

---
+++ WebPersonal_MVC/Controllers/UsuarioController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebPersonal_MVC.Models;
+using WebPersonal_MVC.Models.Dto;
+using WebPersonal_MVC.Services.IServices;
+
+namespace WebPersonal_MVC.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuarioController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginRequestDto modelo)
+        {
+            return View();
+        }
+
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Registrar(RegistroRequestDto modelo)
+        {
+            var response = await _usuarioService.Registrar<APIResponse>(modelo);
+            if(response == null && response.IsExitoso)
+            {
+                return RedirectToAction("login");
+            }
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+        public IActionResult AccesoDenegado()
+        {
+            return View();
+        }
+
+    }
+}
--- WebPersonal_MVC/obj/Debug/net8.0/WebPersonal_MVC.AssemblyInfo.cs
+++ WebPersonal_MVC/obj/Debug/net8.0/WebPersonal_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebPersonal_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945667c0ffb5ecaed0428e6e75cf1e25ac2319dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a480c778bf7ffcb9bb387126dbd70e18c58208ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebPersonal_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebPersonal_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
